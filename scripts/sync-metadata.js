9260e57d113da69f7a80010cbe455ec0e3030902
LukasHechenberger <admin@ls-age.com>
2020-01-24 18:35:28 +0100

    chore(package): Add repository metadata

---
+++ scripts/sync-metadata.js
@@ -0,0 +1,45 @@
+const { promises: fs } = require('fs');
+const { join } = require('path');
+const rootPackage = require('../package.json');
+
+const readJson = path => fs.readFile(path, 'utf8').then(JSON.parse);
+const writeJson = (path, data) => fs.writeFile(path, JSON.stringify(data, null, '  '));
+
+async function findPackages() {
+  const dirs = (await fs.readdir('./packages', { withFileTypes: true })).filter(ent =>
+    ent.isDirectory()
+  );
+
+  return Promise.all(
+    dirs.map(async d => {
+      const path = join('./packages', d.name);
+      const packagePath = join(path, 'package.json');
+
+      return {
+        path,
+        packagePath,
+        package: await readJson(packagePath),
+      };
+    })
+  );
+}
+
+function updateMetadata(pkg) {
+  const updated = pkg.package;
+  updated.repository = rootPackage.repository;
+  updated.bugs = rootPackage.bugs;
+  updated.homepage = rootPackage.homepage.replace('#readme', `/tree/master/${pkg.path}#readme`);
+
+  writeJson(pkg.packagePath, updated);
+}
+
+async function run() {
+  const packages = await findPackages();
+
+  await Promise.all(packages.map(pkg => updateMetadata(pkg)));
+}
+
+run().catch(error => {
+  console.error(error);
+  process.exitCode = 1;
+});
