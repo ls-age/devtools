c127f3df8c8fe83357be9b774af4d1d42b64ce4e
LukasHechenberger <admin@ls-age.com>
2020-01-24 17:38:10 +0100

    feat: Add rollup-plugin-unused

---
+++ packages/rollup-plugin-unused/rollup.config.js
@@ -0,0 +1,14 @@
+import { builtinModules } from 'module';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import { dependencies, main } from './package.json';
+
+export default {
+  input: './src/index.ts',
+  external: [...builtinModules, ...Object.keys(dependencies)],
+  plugins: [resolve(), typescript()],
+  output: {
+    format: 'cjs',
+    file: main,
+  },
+};
---
+++ packages/rollup-plugin-unused/src/index.ts
@@ -0,0 +1,47 @@
+import { join, relative } from 'path';
+import { EOL } from 'os';
+import globby from 'globby';
+import { createFilter } from '@rollup/pluginutils';
+
+export default function unusedPlugin({
+  extensions = ['.js'],
+  include: _include,
+  exitCode = 1,
+  exclude,
+}) {
+  const cwd = process.cwd();
+  const include = _include || extensions.map(ext => `src/**/*${ext}`);
+  const watching = Boolean(process.env.ROLLUP_WATCH);
+  const filter = createFilter(include, exclude);
+
+  let files: Set<string>;
+  let totalFiles;
+
+  return {
+    name: 'unused-files',
+    async buildStart() {
+      files = new Set((await globby(include)).map(n => join(cwd, n)).filter(filter));
+
+      totalFiles = files.size;
+
+      if (totalFiles === 0) {
+        this.warn(`No matching files`);
+      }
+    },
+    load(id) {
+      files.delete(id);
+    },
+    buildEnd() {
+      if (files.size > 0) {
+        this.warn(`${files.size} of ${totalFiles} files are unused`);
+
+        if (!watching) {
+          process.exitCode = exitCode;
+          console.log(
+            ['Unused files:', ...Array.from(files).map(f => relative(cwd, f))].join(`${EOL}- `)
+          );
+        }
+      }
+    },
+  };
+}
