9ddfb10ccfd4966c0a04996a6348e9e92e37611c
LukasHechenberger <LukasHechenberger@users.noreply.github.com>
2020-07-30 20:47:51 +0200

    feat: Implement update-section module

---
+++ packages/update-section/src/lib/comment-patterns.ts
@@ -0,0 +1,28 @@
+export interface CommentPattern {
+  /** The start and stop sequence of a comment */
+  pattern: [string, string];
+  /** If this language needs newlines between comments and content */
+  wrap?: boolean;
+}
+
+interface CommentPatternMapping extends CommentPattern {
+  extensions: string[];
+}
+
+const languages: CommentPatternMapping[] = [
+  {
+    extensions: ['.html', '.htm'],
+    pattern: ['<!--', '-->'],
+  },
+  {
+    extensions: ['.md', '.markdown'],
+    pattern: ['<!--', '-->'],
+    wrap: true,
+  },
+];
+
+const commentPatterns: Map<string, CommentPattern> = new Map(
+  languages.flatMap(language => language.extensions.map(extension => [extension, language]))
+);
+
+export default commentPatterns;
