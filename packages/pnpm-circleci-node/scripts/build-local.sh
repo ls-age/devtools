bf5b2d38b3f528ab66032b403e5238dda352529e
Lukas Hechenberger <5802656+LukasHechenberger@users.noreply.github.com>
2022-07-13 15:48:16 +0200

    chore: Move config

--- packages/pnpm-circleci-node/scripts/build-local.sh
+++ packages/pnpm-circleci-node/scripts/build-local.sh
@@ -1,8 +1,12 @@
 set -eo pipefail
 
+# Config
+export NODE_VERSION="16.13.1"
+export PNPM_VERSION="7.5.1"
+
 export DOCKER_REPO=lsage/pnpm-circleci-node
 export IMAGE_NAME=lsage/pnpm-circleci-node:latest
 
-docker build -t $IMAGE_NAME .
+docker build --build-arg NODE_VERSION=$NODE_VERSION --build-arg PNPM_VERSION=$PNPM_VERSION -t $IMAGE_NAME .
 
 sh ./hooks/post_push
